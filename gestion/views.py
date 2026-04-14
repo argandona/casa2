953f3f17d5d39c0765792968b7f4874bd23d6920
aargandona <aargandona.m@gmail.com>
2026-04-14 08:52:40 -0500

    LDS2

--- gestion/views.py
+++ gestion/views.py
@@ -80,7 +80,7 @@ def cargar_excel_suministros(request):
                 'LATITUD': 'Latitud',
                 'LONGITUD': 'Longitud',
                 'OBSERVACION_CONTRATISTA': 'Observación Contratista',
-                'Observación_LDS':'Observación LDS',
+                'Observacion_LDS':'Observación LDS',
                 'ESTADO': 'Estado',
             }
             
